e56274475598b304fb001e6f410b50c82ee441b8
Tariq M Nasim <Musab_Abdullah>
2026-06-16 23:47:55 -0700

    added triangle file

---
+++ pyramid_triangle.py
@@ -0,0 +1,99 @@
+# Types of lines
+# Normal lines:
+def line(n):
+    for i in range(n):
+        print("* ", end="")
+
+def line_without_space(n):
+    for i in range(n):
+        print("*", end="")
+
+# Line's with spaces in between them
+def e_line(e):
+    for i in range(0,e):
+        if i == 0 or i == e-1:
+            print ("* ", end="")
+        else:
+            print("  ", end="")
+
+def e_line_without_space(e):
+    for i in range(0,e):
+        if i == 0 or i == e-1:
+            print ("*", end="")
+        else:
+            print("  ", end="")
+
+
+# Write a function named (print_triangle_left_aligned())
+# Example Input: 3 (integer)
+# Example output:
+# * * *
+# * *
+# *
+
+# line(9)
+
+# This triangle prints like this:
+# input == 3
+# output:
+# * * *
+# * *
+# *
+# Note: only works for {line()}, {e_line()}
+def print_triangle_left_aligned(m):
+    for i in range(m):
+        line(m-i)
+        print("")
+
+# print_triangle_left_aligned(7)
+# Below is for experimenting
+
+def experimenting_w_triangles(m):
+    for i in range(m):
+        if i == 0 or m:
+            line(m-1)
+        else:
+            line(m-1)
+        print("")
+
+# experimenting_w_triangles(6)
+
+# Middle triangle (only for one space lines):
+# Input == 7
+# Output:
+# * * * * * * * 
+#  * * * * * * 
+#   * * * * * 
+#    * * * * 
+#     * * * 
+#      * * 
+#       * 
+def middle_triangle(m):
+    for i in range(m):
+        for j in range(i):
+            print(" ", end="")
+        line(m-i)
+        print("")
+
+# This triangle prints like this:
+# input == 3
+# output:
+# * * *
+#   * *
+#     *
+# Note: there must be a space for each variable in the line including spaces
+def print_triangle_right_aligned(m):
+    for i in range(m):
+        for j in range(i):
+            print("  ", end="")
+        line(m-i)
+        print("")
+        
+        
+# print_triangle_right_aligned(7)
+# Work in progress
+def normal_triangle(m):
+    for i in range(m):
+        for j in range(i):
+            print(" ", end="")
+        print("")
